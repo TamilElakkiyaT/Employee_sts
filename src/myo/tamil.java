0223a1c6730c2c43a2723ec7bdae73c928f7b30c
TamilElakkiyaT <elakkiyathalapathi1609@gmail.com>
2025-05-22 17:09:35 +0530

    Commit message

--- src/myo/tamil.java
+++ src/myo/tamil.java
@@ -3,7 +3,7 @@ package myo;
 public class tamil {
 
 	public static void main(String[] args) {
-	System.out.println("he");	// TODO Auto-generated method stub
+	System.out.print("he");	// TODO Auto-generated method stub
 
 	}
 
