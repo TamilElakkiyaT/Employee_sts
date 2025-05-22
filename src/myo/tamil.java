5b368d1641290c453d1be03c28edb5e67e8b28d2
TamilElakkiyaT <elakkiyathalapathi1609@gmail.com>
2025-05-22 17:30:51 +0530

    Apply stashed changes

--- src/myo/tamil.java
+++ src/myo/tamil.java
@@ -3,7 +3,7 @@ package myo;
 public class tamil {
 
 	public static void main(String[] args) {
-	System.out.print("he");	// TODO Auto-generated method stub
+	System.out.println("hello");	// TODO Auto-generated method stub
 
 	}
 
