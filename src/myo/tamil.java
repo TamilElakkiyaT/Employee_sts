3a080f1fb35aa4328882234420f6ea8bba47a67c
TamilElakkiyaT <elakkiyathalapathi1609@gmail.com>
2025-05-22 17:32:42 +0530

    newchange

--- src/myo/tamil.java
+++ src/myo/tamil.java
@@ -3,7 +3,7 @@ package myo;
 public class tamil {
 
 	public static void main(String[] args) {
-	System.out.println("hello");	// TODO Auto-generated method stub
+	System.out.println("tamil elakkiya");	// TODO Auto-generated method stub
 
 	}
 
